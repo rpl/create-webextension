9721ccf9b058430d4041cac60beb407e5024173c
Cesar Carruitero <cesar@mozilla.pe>
2018-11-05 12:46:21 -0500

    allow customize manifest

---
+++ permissionOptions.js
@@ -0,0 +1,38 @@
+const availablePermissions = [
+  'activeTab',
+  'alarms',
+  'bookmarks',
+  'browsingData',
+  'contextMenus',
+  'contextualIdentities',
+  'cookies',
+  'downloads',
+  'downloads.open',
+  'history',
+  'identity',
+  'idle',
+  'management',
+  'nativeMessaging',
+  'notifications',
+  'sessions',
+  'storage',
+  'tabs',
+  'topSites',
+  'webNavigation',
+  'webRequest',
+  'webRequestBlocking'
+];
+
+function getChoices(list) {
+  return list.map(item => {
+    return {
+      value: item,
+      name: item,
+      checked: false
+    };
+  });
+}
+
+module.exports = {
+  permissionChoices: getChoices(availablePermissions)
+};
